07c73c9db3c8caab237ddc20e72e0a40cc11641c
User <user@localhost.localdomain>
2021-03-29 13:06:52 -0400

    Add moving paddles.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use ggez::{conf, GameResult};
 
 mod pong;
 
-use crate::pong::Pong;
+use crate::pong::{Paddle, Pong};
+
+const GAME_WIDTH: f32 = 1920.0;
+const GAME_HEIGHT: f32 = 1080.0;
 
 fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("name", "author")
         .window_setup(conf::WindowSetup::default().vsync(false))
-        .window_mode(conf::WindowMode::default().dimensions(1920.0, 1080.0));
+        .window_mode(conf::WindowMode::default().dimensions(GAME_WIDTH, GAME_HEIGHT));
 
     let (ctx, event_loop) = &mut cb.build()?;
 
@@ -16,11 +19,11 @@ fn main() -> GameResult {
         clicks: 0,
         mouse_x: 0.0,
         mouse_y: 0.0,
-        rect_x: (1920 / 2) as f32,
-        rect_y: (1080 / 2) as f32,
         velocity_x: 240.0,
         dt: 1.0f64 / 60.0f64,
         debug_mode: false,
+        left_paddle: Paddle::new(GAME_WIDTH, GAME_HEIGHT, true),
+        right_paddle: Paddle::new(GAME_WIDTH, GAME_HEIGHT, false),
     };
 
     event::run(ctx, event_loop, pong)
--- src/pong.rs
+++ src/pong.rs
@@ -1,3 +1,4 @@
+use ggez::graphics::DrawMode;
 use ggez::input::keyboard::{KeyCode, KeyMods};
 use ggez::input::mouse::MouseButton;
 
@@ -7,6 +8,71 @@ use ggez::nalgebra::Point2;
 use ggez::timer;
 use ggez::*;
 use ggez::{Context, GameResult};
+
+const PADDLE_WIDTH: f32 = 32.0;
+const PADDLE_HEIGHT: f32 = 128.0;
+
+pub struct Pong {
+    pub clicks: usize,
+    pub mouse_x: f32,
+    pub mouse_y: f32,
+    pub velocity_x: f32,
+    pub dt: f64,
+    pub debug_mode: bool,
+    pub left_paddle: Paddle,
+    pub right_paddle: Paddle,
+}
+
+pub struct Paddle {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+    pub speed: f32,
+    pub direction: f32,
+}
+
+impl Paddle {
+    pub fn new(game_width: f32, game_height: f32, left: bool) -> Paddle {
+        Paddle {
+            x: match left {
+                true => 0.0,
+                false => game_width - PADDLE_WIDTH,
+            },
+            y: ((game_height - PADDLE_HEIGHT) / 2.0),
+            w: PADDLE_WIDTH,
+            h: PADDLE_HEIGHT,
+            speed: 350.0,
+            direction: 0.0,
+        }
+    }
+}
+
+impl Pong {
+    pub fn simulate(&mut self, time: f64) {
+        let distance = self.left_paddle.speed as f64 * time;
+        self.left_paddle.y = self.left_paddle.y - (distance as f32 * self.left_paddle.direction);
+
+        let distance = self.right_paddle.speed as f64 * time;
+        self.right_paddle.y = self.right_paddle.y - (distance as f32 * self.right_paddle.direction)
+    }
+}
+
+fn build_rectangle(
+    ctx: &mut Context,
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+) -> GameResult<graphics::Mesh> {
+    let mb = &mut graphics::MeshBuilder::new();
+
+    let rect = graphics::Rect::new(x, y, w, h);
+
+    mb.rectangle(DrawMode::fill(), rect, graphics::WHITE);
+
+    mb.build(ctx)
+}
 
 impl EventHandler for Pong {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
@@ -30,22 +96,35 @@ impl EventHandler for Pong {
         let mouse_display =
             graphics::Text::new(format!("Mouse: ({}, {})", self.mouse_x, self.mouse_y));
 
-        graphics::clear(ctx, graphics::WHITE);
+        graphics::clear(ctx, graphics::BLACK);
 
         if self.debug_mode {
-            graphics::draw(ctx, &fps_display, (Point2::new(0.0, 0.0), graphics::BLACK))?;
+            graphics::draw(ctx, &fps_display, (Point2::new(0.0, 0.0), graphics::WHITE))?;
             graphics::draw(
                 ctx,
                 &mouse_display,
-                (Point2::new(0.0, 20.0), graphics::BLACK),
+                (Point2::new(0.0, 20.0), graphics::WHITE),
             )?;
         }
 
-        let rect = graphics::Rect::new(self.rect_x, self.rect_y, 50.0, 50.0);
-        let rect_mesh =
-            graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), rect, graphics::BLACK)?;
+        let left_rectangle = build_rectangle(
+            ctx,
+            self.left_paddle.x,
+            self.left_paddle.y,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+        )?;
 
-        graphics::draw(ctx, &rect_mesh, DrawParam::default())?;
+        let right_rectangle = build_rectangle(
+            ctx,
+            self.right_paddle.x,
+            self.right_paddle.y,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+        )?;
+
+        graphics::draw(ctx, &left_rectangle, DrawParam::default())?;
+        graphics::draw(ctx, &right_rectangle, DrawParam::default())?;
 
         graphics::present(ctx)
     }
@@ -72,25 +151,11 @@ impl EventHandler for Pong {
         match keycode {
             KeyCode::F1 => self.debug_mode = !self.debug_mode,
             KeyCode::Escape => event::quit(ctx),
+            KeyCode::W => self.left_paddle.direction = 1.0,
+            KeyCode::S => self.left_paddle.direction = -1.0,
+            KeyCode::Up => self.right_paddle.direction = 1.0,
+            KeyCode::Down => self.right_paddle.direction = -1.0,
             default => (),
         }
-    }
-}
-
-pub struct Pong {
-    pub clicks: usize,
-    pub mouse_x: f32,
-    pub mouse_y: f32,
-    pub rect_x: f32,
-    pub rect_y: f32,
-    pub velocity_x: f32,
-    pub dt: f64,
-    pub debug_mode: bool,
-}
-
-impl Pong {
-    pub fn simulate(&mut self, time: f64) {
-        let distance = self.velocity_x as f64 * time;
-        self.rect_x = self.rect_x % 1920.0 + distance as f32;
     }
 }
